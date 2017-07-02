0e9a78ba945f49ad42de15cc7395fd6ffc1870ff
andreapaiola <andrea.paiola@digintel.it>
2017-07-02 10:58:49 +0200

    Big file example

--- examples/big.php
+++ examples/big.php
@@ -44,12 +44,20 @@ require_once '../P2p-Cdn.php';
 $p2pCdn = new P2p_Cdn((isset($_SERVER['HTTPS']) ? "https" : "http") . '://'.$_SERVER['HTTP_HOST'].substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1).'t.php?file=');
 
 $file1=$p2pCdn->file('ubuntu-17.04-desktop-amd64.iso');
+
+/* Ubuntu http://ba.mirror.garr.it/mirrors/ubuntu-releases/17.04/ubuntu-17.04-desktop-amd64.iso.torrent */
+
 ?>
 
 <?php echo $p2pCdn->render(array('file1'=>$file1),'<a href="{file1}" target="_blank">BIG FILE ubuntu-17.04-desktop-amd64.iso (not versioned)</a>'); ?>
 
 <script src="webtorrent.min.js"></script>
 <script src="../P2p-Cdn.js"></script>
+<script>
+    // Custom endpoint
+    // Look at the scope of the variable!
+    var P2PCDNEndpoint = '<?php echo $p2pCdn->getEndpoint(); ?>';
+</script>
 
 </body>
 </html>
